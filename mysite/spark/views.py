13f2c071e8defab722cb9712c176317e0020bb3b
hiroya-ie <e195738@ie.u-ryukyu.ac.jp>
2021-09-29 14:58:26 +0900

    support HTML view

--- mysite/spark/views.py
+++ mysite/spark/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 from . import randomWords
 
 def index(request):
-    out = randomWords.randomA()
-    return HttpResponse(out)
+    #out = randomWords.randomA()
+    #return HttpResponse(out)
+    content = {'message': 'こんにちは！Djangoテンプレート！'}
+    return render(request, 'spark/index.html', content)
