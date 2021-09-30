1a0e07029503fd375d7ca420ff7964b18e7f311d
yabikuhirohumi <e195762@ie.u-ryukyu.ac.jp>
2021-09-30 15:18:22 +0900

    DBの準備

--- mysite/spark/models.py
+++ mysite/spark/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 class WordModel(models.Model):
     word = models.CharField(max_length = 50)
+
+WordModel(word = "aaaaa")
+
--- mysite/spark/randomWords.py
+++ mysite/spark/randomWords.py
@@ -1,5 +1,9 @@
 #テキストをランダムで表示する
 import random
+import lxml.html
+from pytrends.request import TrendReq
 
 def randomA():
     return random.choice(["リンゴ","ゴリラ","ラッパ","パンツ"])
+
+#pytrends.trending_searches(pn='japan')
