969c4a4a4a812d984beedf17e36a50222802157a
yabikuhirohumi <e195762@ie.u-ryukyu.ac.jp>
2021-10-05 17:11:41 +0900

    test

--- spark/randomWords.py
+++ spark/randomWords.py
@@ -1,9 +1,13 @@
 #テキストをランダムで表示する
 import random
-import lxml.html
-from pytrends.request import TrendReq
+import sqlite3
+#import lxml.html
+#from pytrends.request import TrendReq
 
+conn = sqlite3.connect("wnjpn.db")
 def randomA():
     return random.choice(["リンゴ","ゴリラ","ラッパ","パンツ"])
 
 #pytrends.trending_searches(pn='japan')
+
+#テスト
