c04845f7872807f8f6e5e19674c9a5f11aa20396
hiroya-ie <e195738@ie.u-ryukyu.ac.jp>
2021-10-11 20:42:22 +0900

    optimize heroku again

--- spark/randomWords.py
+++ spark/randomWords.py
@@ -14,7 +14,7 @@ def randomA():
 import sys, sqlite3
 from collections import namedtuple
 from pprint import pprint
-conn = sqlite3.connect("./wnjpn.db")
+conn = sqlite3.connect("wnjpn.db")
 cur = conn.execute("select * from word where lang = 'jpn' ORDER BY RANDOM() LIMIT 1000;")
 nwn_words_list = [ record[2] for record in cur.fetchall()]
 
