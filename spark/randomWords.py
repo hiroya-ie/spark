cf8814e892e095de2fae29263fb4c0bc2d945a4d
hiroya-ie <e195738@ie.u-ryukyu.ac.jp>
2021-10-11 21:05:20 +0900

    delete mention DB

--- spark/randomWords.py
+++ spark/randomWords.py
@@ -10,14 +10,6 @@ def randomA():
 
 
 
-#日本語ワードネットのデータベースからリスト作成
-import sys, sqlite3
-from collections import namedtuple
-from pprint import pprint
-conn = sqlite3.connect("wnjpn.db")
-cur = conn.execute("select * from word where lang = 'jpn' ORDER BY RANDOM() LIMIT 1000;")
-nwn_words_list = [ record[2] for record in cur.fetchall()]
-
 
 
 #連想語サイトからスクレイピングしてリスト作成
