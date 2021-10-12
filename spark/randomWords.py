ce5f9c131e2d37c67e1e6dc9958fe5addf8df925
hiroya-ie <e195738@ie.u-ryukyu.ac.jp>
2021-10-12 17:17:26 +0900

    change randomwords to PyTrends

--- spark/randomWords.py
+++ spark/randomWords.py
@@ -4,27 +4,21 @@ import random
 # def randomA():
 #     return random.choice(["リンゴ","ゴリラ","ラッパ","パンツ"])
 
-
 def randomA():
-    return random.choice(rensou_words_list)
+    return random.choice(trend_words_list)
 
+
+#pytrendからリスト作成
+from pytrends.request import TrendReq
+pytrend = TrendReq(hl='ja-jp',tz=540)
+trend_words_list = pytrend.trending_searches(pn='japan')[0].to_list()
 
 
 
 
 #連想語サイトからスクレイピングしてリスト作成
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-html = urlopen("https://renso-ruigo.com")
-data = html.read()
-html = data.decode('utf-8')
-soup = BeautifulSoup(html, 'html.parser')
-links = soup.find_all("a")
-rensou_words_list=[]
 
-for a in links:
-    text = a.text
-    rensou_words_list.append(text)
 
-del rensou_words_list[:4]
-del rensou_words_list[-55:]
+
+
+
