18b72162a627cadf8e10e37c757176bcd70db7dd
hiroya-ie <e195738@ie.u-ryukyu.ac.jp>
2021-10-13 14:23:21 +0900

    modified views

--- spark/views.py
+++ spark/views.py
@@ -6,9 +6,6 @@ def index(request):
     wordlist=randomWords.randomA()
     
     content = {
-        'w1': wordlist[0],
-        'w2': wordlist[1],
-        'w3': wordlist[2],
         'values': wordlist
         }
     return render(request, 'spark/index.html', content)
