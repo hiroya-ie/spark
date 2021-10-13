57441421b146b51d6472716f9ae8fc079f8c76a6
大城　璃功 <e195763@ie.u-ryukyu.ac.jp>
2021-10-13 13:58:39 +0900

    changed randomWords.py

--- spark/randomWords.py
+++ spark/randomWords.py
@@ -4,9 +4,10 @@ import random
 # def randomA():
 #     return random.choice(["リンゴ","ゴリラ","ラッパ","パンツ"])
 
+l = ["リンゴ","ゴリラ","ラッパ","パンツ"]
 
 def randomA():
-    return random.choice(trend_words_list)
+    return random.sample(l, 3)
 
 
 
@@ -14,7 +15,7 @@ def randomA():
 import sys, sqlite3
 from collections import namedtuple
 from pprint import pprint
-conn = sqlite3.connect("./wnjpn.db")
+conn = sqlite3.connect("./spark/wnjpn.dbのコピー")
 cur = conn.execute("select * from word where lang = 'jpn' ORDER BY RANDOM() LIMIT 1000;")
 nwn_words_list = [ record[2] for record in cur.fetchall()]
 
@@ -27,24 +28,28 @@ trend_words_list = pytrend.trending_searches(pn='japan')[0].to_list()
 
 
 
-
-
 #連想語サイトからスクレイピングしてリスト作成
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-html = urlopen("https://renso-ruigo.com")
-data = html.read()
-html = data.decode('utf-8')
-soup = BeautifulSoup(html, 'html.parser')
-links = soup.find_all("a")
-rensou_words_list=[]
+wlist=[]
 
-for a in links:
-    text = a.text
-    rensou_words_list.append(text)
+#スクレイピング5回やる
+for i in range(5):
+    html = urlopen("https://renso-ruigo.com")
+    data = html.read()
+    html = data.decode('utf-8')
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.find_all("a")
+    rensou_words_list=[a.text for a in links]
+    del rensou_words_list[:4]
+    del rensou_words_list[-55:]
+    wlist.extend(rensou_words_list)
 
-del rensou_words_list[:4]
-del rensou_words_list[-55:]
+#トレンドのリストと合体
+trend_words_list.extend(wlist)
+
+
+
 
 
 
