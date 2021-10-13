87f1c6ae0044840d5c035077f6159fd961c58b0c
大城　璃功 <e195763@ie.u-ryukyu.ac.jp>
2021-10-13 13:59:38 +0900

    changed views.py

--- spark/views.py
+++ spark/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render
 from . import randomWords
 
 def index(request):
+    wordlist=randomWords.randomA()
     content = {
-        'w1': randomWords.randomA(),
-        'w2': randomWords.randomA(),
-        'w3': randomWords.randomA()
+        'w1': wordlist[0],
+        'w2': wordlist[1],
+        'w3': wordlist[2]
         }
     return render(request, 'spark/index.html', content)
