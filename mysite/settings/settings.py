71c35a8a1da5f88d6769585decd9511f5d876268
hiroya-ie <e195738@ie.u-ryukyu.ac.jp>
2021-11-26 13:34:03 +0900

    set for VM-open-env

--- mysite/settings/settings.py
+++ mysite/settings/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
+import environ
+
+env = environ.Env()
+env.read_env('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3!j54hjx(l5^dwe_p0a8kzq68nf@o3q--ioba(mh5gd!tz-4!u'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['spark-e195738.st.ie.u-ryukyu.ac.jp']
 
@@ -140,9 +144,9 @@ DATABASES = {
 }
 
 # 追記
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+#import dj_database_url
+#db_from_env = dj_database_url.config()
+#DATABASES['default'].update(db_from_env)
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -152,4 +156,5 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static')
 )
