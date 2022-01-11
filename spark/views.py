b9f41c8cd16f0f3c47e082b0ef302c7214629838
hiroya-ie <e195738@ie.u-ryukyu.ac.jp>
2022-01-11 14:08:45 +0900

    view4 -> views(index)

--- spark/views.py
+++ spark/views.py
@@ -3,19 +3,33 @@ from . import randomWords
 from django.http import JsonResponse
 
 
-def index(request):
-    wordlist=randomWords.randomA()
+
+def view4(request):
+    ie_wordlist, trend_rensou_wordlist, wiki = randomWords.randomC()
+
+    content = {
+        'word_ie1': ie_wordlist[0],
+        'word_ie2': ie_wordlist[1],
+        'word_ie3': ie_wordlist[2],
+        'word_rand1': trend_rensou_wordlist[0],
+        'word_rand2': trend_rensou_wordlist[1],
+        'word_rand3': trend_rensou_wordlist[2],
+        'word_rand4': trend_rensou_wordlist[3],
+        'word_rand5': trend_rensou_wordlist[4],
+        'word_rand6': trend_rensou_wordlist[5],
+        'word_rand7': trend_rensou_wordlist[6],
+        'word_rand8': trend_rensou_wordlist[7],
+        'word_rand9': trend_rensou_wordlist[8],
+        'wiki':wiki
+
+        }
+    return render(request, 'spark/view4.html', content)
+
+
+def ajax_wiki(request):
+    ie_wordlist, trend_rensou_wordlist, wiki = randomWords.randomC()
     
     content = {
-        'values': wordlist
-        }
-    return render(request, 'spark/index.html', content)
-
-
-def index(request):
-    ie_wordlist, trend_rensou_wordlist = randomWords.randomA()
-
-    content = {
         'word_ie1': ie_wordlist[0],
         'word_ie2': ie_wordlist[1],
         'word_ie3': ie_wordlist[2],
@@ -28,25 +42,7 @@ def index(request):
         'word_rand7': trend_rensou_wordlist[6],
         'word_rand8': trend_rensou_wordlist[7],
         'word_rand9': trend_rensou_wordlist[8],
-
-        }
-    return render(request, 'spark/index.html', content)
-
-def ajax_number(request):
-    ie_wordlist, trend_rensou_wordlist = randomWords.randomA()
-
-    content = {
-        'word_ie1': ie_wordlist[0],
-        'word_ie2': ie_wordlist[1],
-        'word_ie3': ie_wordlist[2],
-        'word_rand1': trend_rensou_wordlist[0],
-        'word_rand2': trend_rensou_wordlist[1],
-        'word_rand3': trend_rensou_wordlist[2],
-        'word_rand4': trend_rensou_wordlist[3],
-        'word_rand5': trend_rensou_wordlist[4],
-        'word_rand6': trend_rensou_wordlist[5],
-        'word_rand7': trend_rensou_wordlist[6],
-        'word_rand8': trend_rensou_wordlist[7],
-        'word_rand9': trend_rensou_wordlist[8],
+        'wiki': wiki
+    
         }
     return JsonResponse(content)
