d2c8a5863618864027c0a4223f55d9728c11ae15
大城　璃功 <e195763@ie.u-ryukyu.ac.jp>
2022-01-14 02:02:05 +0900

    Keepのスクロールテスト

--- spark/view5.py
+++ spark/view5.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 
 
 def view5(request):
-    ie_wordlist, trend_rensou_wordlist = randomWords.randomA()
+    ie_wordlist, trend_rensou_wordlist, wiki = randomWords.randomC()
 
     content = {
         'word_ie1': ie_wordlist[0],
@@ -20,6 +20,28 @@ def view5(request):
         'word_rand7': trend_rensou_wordlist[6],
         'word_rand8': trend_rensou_wordlist[7],
         'word_rand9': trend_rensou_wordlist[8],
+        'wiki':wiki
 
         }
     return render(request, 'spark/view5.html', content)
+
+def ajax_wiki(request):
+    ie_wordlist, trend_rensou_wordlist, wiki = randomWords.randomC()
+    
+    content = {
+        'word_ie1': ie_wordlist[0],
+        'word_ie2': ie_wordlist[1],
+        'word_ie3': ie_wordlist[2],
+        'word_rand1': trend_rensou_wordlist[0],
+        'word_rand2': trend_rensou_wordlist[1],
+        'word_rand3': trend_rensou_wordlist[2],
+        'word_rand4': trend_rensou_wordlist[3],
+        'word_rand5': trend_rensou_wordlist[4],
+        'word_rand6': trend_rensou_wordlist[5],
+        'word_rand7': trend_rensou_wordlist[6],
+        'word_rand8': trend_rensou_wordlist[7],
+        'word_rand9': trend_rensou_wordlist[8],
+        'wiki': wiki
+    
+        }
+    return JsonResponse(content)
