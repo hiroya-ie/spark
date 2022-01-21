c2ad24f189ff7b790c63b54a52522a022e0ed6ca
hiroya-ie <e195738@ie.u-ryukyu.ac.jp>
2022-01-21 12:26:15 +0900

    debug moode False -> True

--- mysite/settings/settings.py
+++ mysite/settings/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3!j54hjx(l5^dwe_p0a8kzq68nf@o3q--ioba(mh5gd!tz-4!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['spark-enpit2021.herokuapp.com']
 
