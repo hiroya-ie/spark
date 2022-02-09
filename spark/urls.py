a524340f8eee76a55cf9641acf43875ac6038947
大城　璃功 <e195763@ie.u-ryukyu.ac.jp>
2022-02-09 11:38:05 +0900

    kokusai用

--- spark/urls.py
+++ spark/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-
+from . import kokusai
 
 
 app_name = 'blog'
@@ -11,4 +11,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('ajax-number/', views.ajax_number, name='ajax_number'),
     path('ajax-wiki/', views.ajax_wiki, name='ajax_wiki'),
+    path('kokusai', kokusai.ajax_kokusai, name = 'ajax_kokusai'),
+    path('kokusai', kokusai.kokusai, name = 'kokusai'),
 ]
